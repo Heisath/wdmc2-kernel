7106247d149e656963c844b7f91740463ca4a540
count-doku <jannis@imserv.org>
2020-05-27 22:54:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# wdmc-gen2
+WD My Cloud Gen2 kernel build script
+
+
+WD My Cloud Gen2 aka wdmc-gen2 based on Marvell ARMADA 375
+===================================================
+
+* mainline kernel support
+	tested with 4.18.x / 5.6.x
+	- device tree source
+		armada-375-wdmc-gen2.dts
+	- kernel config
+		kernel-4.18.19.config, kernel-5.6.config kernel-default.config
+
+	- build your own kernel
+        - edit buid_kernel_image.sh , adjust KERNEL_VERSION to match desired kernel version from kernel.org
+		- run build_kernel_image.sh
+        - it will git clone and checkout the kernel 
+        - insert kernel-config and device tree into kernel
+        - try making it with menuconfig 
+        - attach dtb to zImage then mkimage
+        - copy uImage to /boot of your boot partition
+
+	- build-initramfs.sh
+
+		builds a minimal initramfs.  Can boot from kernel commandline,
+		usb-stick 2nd partition, hdd 3rd partition.
+		needs to be placed in /boot/uRamdisk.
+
+        This needs to be run on the wdmycloud. So to get your first boot use the uRamdisk provided!
+		
+
---
+++ build_kernel_image.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+KERNEL_VERSION='5.6'
+
+# we can never know what aliases may be set, so remove them all
+unalias -a
+
+# do preparation steps
+
+# generate output directory
+mkdir -p output/boot
+
+if [ ! -d linux-$KERNEL_VERSION ]; then
+    # git clone linux tree
+    git clone --branch "v$KERNEL_VERSION" --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+
+    # rename directory
+    mv linux-stable linux-$KERNEL_VERSION
+fi
+
+# copy config and dts
+
+if [ ! -f kernel-$KERNEL_VERSION.config ]; then
+    cp kernel-default.config kernel-$KERNEL_VERSION.config
+fi
+
+cp kernel-$KERNEL_VERSION.config linux-$KERNEL_VERSION/.config
+cp armada-375-wdmc-gen2.dts linux-$KERNEL_VERSION/arch/arm/boot/dts/
+
+
+# cd into linux source
+cd linux-$KERNEL_VERSION
+
+makehelp='make CROSS_COMPILE=/opt/arm-marvell-linux-gnueabi/bin/arm-marvell-linux-gnueabi- ARCH=arm'
+
+$makehelp menuconfig
+$makehelp -j2 zImage
+$makehelp armada-375-wdmc-gen2.dtb
+cat arch/arm/boot/zImage arch/arm/boot/dts/armada-375-wdmc-gen2.dtb > zImage_and_dtb
+mkimage -A arm -O linux -T kernel -C none -a 0x00008000 -e 0x00008000 -n 'WDMC-Gen2' -d zImage_and_dtb ../output/boot/uImage-$KERNEL_VERSION
+rm zImage_and_dtb
+
+$makehelp modules
+$makehelp INSTALL_MOD_PATH=../output modules_install
+
+cd ..
+
+cp uRamdisk output/boot/
+
+chmod =rwxrxrx output/boot/uRamdisk
+chmod =rwxrxrx output/boot/uImage-$KERNEL_VERSION
+
+
