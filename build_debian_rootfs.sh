eba207c1dea8ccb97167015f7e48849066cf3def
count-doku <jannis@imserv.org>
2020-10-10 00:00:08 +0200

    Improve rootfs creation

--- build_debian_rootfs.sh
+++ build_debian_rootfs.sh
@@ -10,85 +10,72 @@ exit_with_error()
 # we can never know what aliases may be set, so remove them all
 unalias -a
 
-# do preparation steps
-target_dir="output/rootfs"
-
-# cleanup old
-rm -rf "${target_dir}"
-
 # set options
+target_dir="output"
+rootfs="${target_dir}"/rootfs
 release='buster'
 arch='armhf'
 qemu_binary='qemu-arm-static'
 components='main,contrib'
 includes="ccache,locales,git,ca-certificates,debhelper,rsync,python3,distcc,systemd,init,udev,kmod,bash-completion,busybox,ethtool,dirmngr,hdparm,ifupdown,iproute2,iputils-ping,logrotate,net-tools,nftables,powermgmt-base,procps,rename,resolvconf,rsyslog,ssh,sysstat,update-inetd" #libfile-fcntllock-perl,devscripts,
 mirror_addr="http://httpredir.debian.org/debian/"
+root_pw='1234'
+
+# cleanup old
+rm -rf "${rootfs}"
+rm -rf "${target_dir}"/"${release}"-rootfs.tar.gz
 
 # generate output directory
-mkdir -p $target_dir
+mkdir -p ${rootfs}
 
 echo "### Creating build chroot: $release/$arch"
 
-# 
-
-debootstrap --variant=minbase --arch="${arch}" --foreign --components="${components}" --include="${includes}" "${release}" "${target_dir}" "${mirror_addr}"
-[[ $? -ne 0 || ! -f $target_dir/debootstrap/debootstrap ]] && exit_with_error "### Create chroot first stage failed"
+debootstrap --variant=minbase --arch="${arch}" --foreign --components="${components}" --include="${includes}" "${release}" "${rootfs}" "${mirror_addr}"
+[[ $? -ne 0 || ! -f "${rootfs}"/debootstrap/debootstrap ]] && exit_with_error "### Create chroot first stage failed"
 
 echo "### First stage completed"
 
-cp /usr/bin/$qemu_binary $target_dir/usr/bin/
+cp /usr/bin/"${qemu_binary}" "${rootfs}"/usr/bin/
 
-mkdir -p  $target_dir/usr/share/keyrings/
-cp /usr/share/keyrings/*-archive-keyring.gpg $target_dir/usr/share/keyrings/
+mkdir -p  "${rootfs}"/usr/share/keyrings/
+cp /usr/share/keyrings/*-archive-keyring.gpg "${rootfs}"/usr/share/keyrings/
 
+echo "### Copied qemu and keyring"
 
-chroot "${target_dir}" /bin/bash -c "/debootstrap/debootstrap --second-stage"
-[[ $? -ne 0 || ! -f "${target_dir}"/bin/bash ]] && exit_with_error "### Create chroot second stage failed"
+chroot "${rootfs}" /bin/bash -c "/debootstrap/debootstrap --second-stage"
+[[ $? -ne 0 || ! -f "${rootfs}"/bin/bash ]] && exit_with_error "### Create chroot second stage failed"
 echo "### Second stage completed"
 
-mount -t proc chproc "${target_dir}"/proc
-mount -t sysfs chsys "${target_dir}"/sys
-mount -t devtmpfs chdev "${target_dir}"/dev || mount --bind /dev "${target_dir}"/dev
-mount -t devpts chpts "${target_dir}"/dev/pts
+echo "### Mounting / preparing chroot"
+mount -t proc chproc "${rootfs}"/proc
+mount -t sysfs chsys "${rootfs}"/sys
+mount -t devtmpfs chdev "${rootfs}"/dev || mount --bind /dev "${rootfs}"/dev
+mount -t devpts chpts "${rootfs}"/dev/pts
 
 
-[[ -f $target_dir/etc/locale.gen ]] && sed -i "s/^# en_US.UTF-8/en_US.UTF-8/" $target_dir/etc/locale.gen
-chroot $target_dir /bin/bash -c "locale-gen; update-locale LANG=en_US:en LC_ALL=en_US.UTF-8"
-chroot $target_dir /bin/bash -c "/usr/sbin/update-ccache-symlinks"
+echo "### Applying tweaks"
+[[ -f "${rootfs}"/etc/locale.gen ]] && sed -i "s/^# en_US.UTF-8/en_US.UTF-8/" "${rootfs}"/etc/locale.gen
+chroot "${rootfs}" /bin/bash -c "locale-gen; update-locale LANG=en_US:en LC_ALL=en_US.UTF-8"
+chroot "${rootfs}" /bin/bash -c "/usr/sbin/update-ccache-symlinks"
 
-if [[ -f $target_dir/etc/default/console-setup ]]; then
-	sed -e 's/CHARMAP=.*/CHARMAP="UTF-8"/' -e 's/FONTSIZE=.*/FONTSIZE="8x16"/' \
-		-e 's/CODESET=.*/CODESET="guess"/' -i $target_dir/etc/default/console-setup
-	eval 'LC_ALL=C LANG=C chroot $target_dir /bin/bash -c "setupcon --save"'
-fi
+#if [[ -f "${rootfs}"/etc/default/console-setup ]]; then
+#	sed -e 's/CHARMAP=.*/CHARMAP="UTF-8"/' -e 's/FONTSIZE=.*/FONTSIZE="8x16"/' \
+#		-e 's/CODESET=.*/CODESET="guess"/' -i "${rootfs}"/etc/default/console-setup
+#	eval 'LC_ALL=C LANG=C chroot $rootfs /bin/bash -c "setupcon --save"'
+#fi
 
-chroot "${target_dir}" /bin/bash -c "apt-get -y update"
-chroot "${target_dir}" /bin/bash -c "apt-get -y install systemd init udev kmod bash-completion busybox ethtool dirmngr hdparm ifupdown iproute2 iproute iftables iputils-ping logrotate net-tools nftables powermgmt-base procps rename resolvconf rsyslog ssh sysstat update-inetd"
-
-chroot "${target_dir}" /bin/bash -c "apt-get -y full-upgrade"
-chroot "${target_dir}" /bin/bash -c "apt-get -y autoremove"
-chroot "${target_dir}" /bin/bash -c "apt-get -y clean"
+chroot "${rootfs}" /bin/bash -c "apt-get -y update"
+chroot "${rootfs}" /bin/bash -c "apt-get -y full-upgrade"
+chroot "${rootfs}" /bin/bash -c "apt-get -y autoremove"
+chroot "${rootfs}" /bin/bash -c "apt-get -y clean"
 
 # set root password
-chroot "${target_dir}" /bin/bash -c "(echo 1234;echo 1234;) | passwd root >/dev/null 2>&1"
+chroot "${rootfs}" /bin/bash -c "(echo ${root_pw};echo ${root_pw};) | passwd root >/dev/null 2>&1"
 
-
-
-while grep -Eq "${target_dir}.*(dev|proc|sys)" /proc/mounts
-do
-	umount -l --recursive "${target_dir}"/dev >/dev/null 2>&1
-	umount -l "${target_dir}"/proc >/dev/null 2>&1
-	umount -l "${target_dir}"/sys >/dev/null 2>&1
-	sleep 5
-done
-
-touch "${target_dir}"/root/.debootstrap-complete
-echo "### Debootstrap complete: ${release}/${arch}"
-
-cat << EOF > ${target_dir}/etc/fstab
+cat << EOF > ${rootfs}/etc/fstab
 # Default mounts
 /dev/sdb2       /               ext4    defaults,noatime,nodiratime,commit=600,errors=remount-ro 0 1
-#/dev/sdb1       /boot           auto    defaults                0       0
+/dev/sdb1       /boot           auto    defaults                0       0
 
 proc            /proc           proc    defaults                0       0
 #devpts         /dev/pts        devpts  defaults,gid5,mode=620  0       0
@@ -104,10 +91,41 @@ sysfs           /sys            sysfs   defaults                0       0
 #/dev/sda1       /mnt/hd-intern  ext4    defaults,noatime        0       1
 EOF
 
-cat << EOF > ${target_dir}/etc/hostname
-wdmycloudusb
+cat << EOF > ${rootfs}/etc/hostname
+wdmycloud
 EOF
 
-cd "${target_dir}"
+cat << EOF > ${rootfs}/etc/network/interfaces
+# interfaces(5) file used by ifup(8) and ifdown(8)
+# Include files from /etc/network/interfaces.d:
+source-directory /etc/network/interfaces.d
+
+# loopback
+iface lo inet loopback
+iface lo inet6 loopback
+
+# main network interface
+auto eth0
+allow-hotplug eth0
+iface eth0 inet dhcp
+#       address 192.168.1.7
+#       netmask 255.255.255.0
+#       gateway 192.168.1.1
+#       dns-nameservers 192.168.1.1
+EOF
+
+echo "### Unmounting"
+while grep -Eq "${rootfs}.*(dev|proc|sys)" /proc/mounts
+do
+	umount -l --recursive "${rootfs}"/dev >/dev/null 2>&1
+	umount -l "${rootfs}"/proc >/dev/null 2>&1
+	umount -l "${rootfs}"/sys >/dev/null 2>&1
+	sleep 5
+done
+
+touch "${rootfs}"/root/.debootstrap-complete
+echo "### Debootstrap complete: ${release}/${arch}"
+
+cd "${rootfs}"
 
 tar -czf ../"${release}"-rootfs.tar.gz .
