8095a881aa21d638592dfdbdf02de38ad67ffdb0
Heisath <jannis@imserv.org>
2021-05-20 17:37:14 +0200

    Start new build script with dialog

--- build-ng.sh
+++ build-ng.sh
@@ -17,67 +17,165 @@ grab_version()
 	echo "${ver[0]:-0}${ver[1]:+.${ver[1]}}${ver[2]:+.${ver[2]}}${ver[3]}"
 }
 
-current_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+display_result() {
+  dialog --title "$1" \
+    --no-collapse \
+    --msgbox "$result" 0 0
+}
 
-# check for whitespace in ${current_dir} and exit for safety reasons
-grep -q "[[:space:]]" <<<"${current_dir}" && { echo "\"${current_dir}\" contains whitespace. Aborting." >&2 ; exit 1 ; }
+display_select() {
+  exec 3>&1
+  selection=$(dialog \
+    --backtitle "$BACKTITLE" \
+    --title "$1" \
+    --clear \
+    --cancel-label "Exit" \
+    --menu "$2" 0 0 0 \
+    "${@:3}" \
+    2>&1 1>&3)
+  exit_status=$?
+  exec 3>&-
+  case $exit_status in
+    $DIALOG_CANCEL)
+      echo "Program terminated."
+      exit
+      ;;
+    $DIALOG_ESC)
+      echo "Program aborted." >&2
+      exit 1
+      ;;
+  esac
+}
 
-cd "${current_dir}" || exit
+display_checklist() {
+  exec 3>&1
+  selection=$(dialog \
+    --backtitle "$BACKTITLE" \
+    --title "$1" \
+    --clear \
+    --cancel-label "Exit" \
+    --checklist "$2" 0 0 0 \
+    "${@:3}" \
+    2>&1 1>&3)
+  exit_status=$?
+  exec 3>&-
 
-############################################################
-### (SUB) DIRECTORIES TO USE
-############################################################
-output_dir="${current_dir}/output"
-rootfs_dir="${output_dir}/rootfs"
-boot_dir="${output_dir}/boot"
-cache_dir="${current_dir}/cache"
+  case $exit_status in
+    $DIALOG_CANCEL)
+      echo "Program terminated."
+      exit
+      ;;
+    $DIALOG_ESC)
+      echo "Program aborted." >&2
+      exit 1
+      ;;
+  esac
+}
 
-current_user="$(stat --format %U "${current_dir}"/.git)"
+read_arguments() {
+    # read command line to replace defaults
+    POSITIONAL=()
+    while [[ $# -gt 0 ]]
+    do
+        key="$1"
+        value="$2"
 
-############################################################
-# Required gcc:
-#  armada370-gcc464_glibc215_hard_armada-GPL.txz (included in git)    FOR KERNEL VERSION <= 5.6
-#  gcc-arm-none-eabi (downloadable via apt)         FOR KERNEL VERSION >= 5.6
-# check toolchain subfolder for these or install via apt
-# Adjust makehelp to match path to your gcc:
-############################################################
-#makehelp='make CROSS_COMPILE=/opt/arm-marvell-linux-gnueabi/bin/arm-marvell-linux-gnueabi- ARCH=arm'   #FOR KERNEL VERSION <= 5.6 (via txz)
-makehelp='make CROSS_COMPILE=/usr/bin/arm-none-eabi- ARCH=arm'                                         #FOR KERNEL VERSION >= 5.6 (via apt)
+        case $key in
+            # feature selection
+            --kernel)
+                BUILD_KERNEL='on'
+                shift;
+            ;;
+            --clean)
+                CLEAN_KERNEL_SRC='on'
+                shift;
+            ;;
+            --config)
+                ALLOW_KERNEL_CONFIG_CHANGES='on'
+                shift;
+            ;;
 
-############################################################
-### CONFIG VALUES FOR THE CREATED ROOTFS AND KERNEL
-############################################################
-release='bullseye'
-arch='armhf'
-qemu_binary='qemu-arm-static'
-components='main,contrib'
-# Adjust package list here
-includes="ccache,locales,git,ca-certificates,debhelper,rsync,python3,distcc,systemd,init,udev,kmod,busybox,ethtool,dirmngr,hdparm,ifupdown,iproute2,iputils-ping,logrotate,net-tools,nftables,powermgmt-base,procps,rename,resolvconf,rsyslog,ssh,sysstat,update-inetd,isc-dhcp-client,isc-dhcp-common,vim,dialog,apt-utils,nano,keyboard-configuration,console-setup,linux-base,cpio,u-boot-tools,bc" 
-mirror_addr="http://httpredir.debian.org/debian/"
 
-# Adjust default root pw
-root_pw='1234'
+            --rootfs)
+                BUILD_ROOT='on'
+                shift;
+            ;;
+            --changes)
+                ALLOW_ROOTFS_CHANGES='on'
+                shift;
+            ;;
+	        --initramfs)
+	            BUILD_INITRAMFS='on'
+	            shift;
+            ;;
+	        
+	    
+            --ghrunner)
+                BUILD_KERNEL='yes'
+                BUILD_INITRAMFS='off'
+                BUILD_ROOT='off'
+                GHRUNNER='on'
+                THREADS=2
+                ALLOW_KERNEL_CONFIG_CHANGES='off'
+                shift;
+            ;;
+            
+            
+            #  config selection
+        
+            --release)
+                release=${value}
+                shift; shift
+            ;;
+            --root-pw)
+                root_pw=${value}
+                shift; shift
+            ;;
+            --hostname)
+                def_hostname=${value}
+                shift; shift;
+            ;;
+            --kernelbranch)
+                kernel_branch=${value}
+                shift; shift;
+            ;;
 
-# Adjust hostname here
-def_hostname='wdmycloud'
-
-# Default kernel branch
-kernel_branch='linux-5.11.y'
-
-############################################################
-### PARAMETERS CHANING THE BEHAVIOUR OF THIS SCRIPT
-############################################################
-THREADS=8
-GHRUNNER='no'
-BUILD_KERNEL='yes'
-BUILD_ROOT='yes'
-BUILD_INITRAMFS='yes'
-ALLOW_ROOTFS_CHANGES='no'
-CLEAN_KERNEL_SRC='yes'
-ALLOW_KERNEL_CONFIG_CHANGES='yes'
+            *)    # unknown option
+                POSITIONAL+=("$1") # save it in an array for later
+                shift # past argument
+            ;;
+        esac
+    done
+}
 
 build_kernel()
 {
+    if [ -z "$kernel_branch" ]; then
+        display_select "Kernel Building" "Please select the Linux Kernel branch to build." \
+            "4.18" "Linux kernel 4.18" \
+            "5.6" "Linux kernel 5.6" \
+            "5.8" "Linux kernel 5.8" \
+            "5.10" "Linux kernel 5.10" \
+            "5.11" "Linux kernel 5.11" \
+            "5.12" "Linux kernel 5.12"
+            
+        ############################################################
+        # Required gcc:
+        #  armada370-gcc464_glibc215_hard_armada-GPL.txz (included in git)    FOR KERNEL VERSION <= 5.6
+        #  gcc-arm-none-eabi (downloadable via apt)         FOR KERNEL VERSION >= 5.6
+        # check toolchain subfolder for these or install via apt
+        # Adjust makehelp to match path to your gcc:
+        ############################################################
+        makehelp='make CROSS_COMPILE=/usr/bin/arm-none-eabi- ARCH=arm'                                         #FOR KERNEL VERSION >= 5.6 (via apt)
+        if [[ $selection == "4.18" ]]; then
+            makehelp='make CROSS_COMPILE=/opt/arm-marvell-linux-gnueabi/bin/arm-marvell-linux-gnueabi- ARCH=arm'   #FOR KERNEL VERSION <= 5.6 (via txz)
+            display_result "Warning" "You have selected Linux Kernel 4.18, this will be build with older armada370 gcc464 toolchain, make sure you have extracted the txz file available in toolchain folder to /opt/"
+        fi
+
+        kernel_branch="linux-$selection.y"
+    fi
+
+
     # do preparation steps
     echo "### Cloning linux kernel $kernel_branch"
 
@@ -181,7 +279,8 @@ build_kernel()
 
     cd "${current_dir}"
     
-    if [[ $GHRUNNER == 'yes' ]]; then
+    # abort point for github runner to keep it from messing with permissions
+    if [[ $GHRUNNER == 'on' ]]; then
         return
     fi
 
@@ -207,6 +306,32 @@ build_kernel()
 
 build_root_fs()
 {
+    if [ -z "$release" ]; then
+        display_select "Rootfs creation" "Please select the Debian release to build." \
+            "buster" "Debian Buster" \
+            "bullseye" "Debian Bullseye" 
+
+        release=$selection
+    fi
+
+    if [ -z "$root_pw" ]; then
+        # Adjust default root pw
+        root_pw='1234'
+    fi
+
+    if [ -z "$def_hostname" ]; then
+        # Adjust hostname here
+        def_hostname='wdmycloud'
+    fi
+
+    # setup some more or less static config variables
+    arch='armhf'
+    qemu_binary='qemu-arm-static'
+    components='main,contrib'
+    # Adjust package list here
+    includes="ccache,locales,git,ca-certificates,debhelper,rsync,python3,distcc,systemd,init,udev,kmod,busybox,ethtool,dirmngr,hdparm,ifupdown,iproute2,iputils-ping,logrotate,net-tools,nftables,powermgmt-base,procps,rename,resolvconf,rsyslog,ssh,sysstat,update-inetd,isc-dhcp-client,isc-dhcp-common,vim,dialog,apt-utils,nano,keyboard-configuration,console-setup,linux-base,cpio,u-boot-tools,bc" 
+    mirror_addr="http://httpredir.debian.org/debian/"
+
     # cleanup old
     rm -rf "${rootfs_dir}"
     rm -rf "${output_dir}"/"${release}"-rootfs.tar.gz
@@ -356,113 +481,86 @@ EOF
 
 }
 
+############################################################
+### Script starts here
+############################################################
 
-# read command line to replace defaults
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-    key="$1"
-    value="$2"
 
-    case $key in
-        --release)
-            release=${value}
-            shift; shift
-        ;;
-        --root-pw)
-            root_pw=${value}
-            shift; shift
-        ;;
-        --hostname)
-            def_hostname=${value}
-            shift; shift;
-        ;;
-        --kernel)
-            kernel_branch=${value}
-            shift; shift;
-        ;;
+# Find dir of build.sh and go there
+current_dir="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+# check for whitespace in ${current_dir} and exit for safety reasons
+grep -q "[[:space:]]" <<<"${current_dir}" && { echo "\"${current_dir}\" contains whitespace. Aborting." >&2 ; exit 1 ; }
+cd "${current_dir}" || exit
 
-        --noclean)
-            CLEAN_KERNEL_SRC='no'
-            shift;
-        ;;
-        --noconfig)
-            ALLOW_KERNEL_CONFIG_CHANGES='no'
-            shift;
-        ;;
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+BACKTITLE="WDMC kernel & rootfs build script"
 
-        --kernelonly)
-            BUILD_ROOT='no'
-            BUILD_INITRAMFS='no'
-            shift;
-        ;;
-        --rootonly)
-            BUILD_KERNEL='no'
-            BUILD_INITRAMFS='no'
-            shift;
-        ;;
-        --nokernel)
-            BUILD_KERNEL='no'
-            shift;
-        ;;
-        --noinitramfs)
-            BUILD_INITRAMFS='no'
-            shift;
-        ;;
+############################################################
+### (SUB) DIRECTORIES TO USE
+############################################################
+output_dir="${current_dir}/output"
+rootfs_dir="${output_dir}/rootfs"
+boot_dir="${output_dir}/boot"
+cache_dir="${current_dir}/cache"
 
-        --changes)
-            ALLOW_ROOTFS_CHANGES='yes'
-            shift;
-        ;;
-	
-        --ghrunner)
-            BUILD_KERNEL='yes'
-            BUILD_INITRAMFS='no'
-            BUILD_ROOT='no'
-            GHRUNNER='yes'
-            THREADS=2
-            ALLOW_KERNEL_CONFIG_CHANGES='no'
-            shift;
-        ;;
-        *)    # unknown option
-            POSITIONAL+=("$1") # save it in an array for later
-            shift # past argument
-        ;;
-    esac
-done
+current_user="$(stat --format %U "${current_dir}"/.git)"
+
+############################################################
+### Begin figuring out configuration
+############################################################
+GHRUNNER='off'
+THREADS=8
+
+# start by reading command line arguments
+read_arguments "$@"
 
 if [[ "${EUID}" != "0" ]] && [[ $GHRUNNER != 'yes' ]]; then
     echo "This script requires root privileges, please rerun using sudo"
     exit 1
 fi
 
-echo '### Build options'
+# if command line has selected neither kernel nor rootfs we can assume, no selection was done and ask user
+if [[ -z $BUILD_KERNEL ]] && [[ -z $BUILD_ROOTFS ]]; then
+    # set sensible defaults
+    BUILD_KERNEL='on'
+    CLEAN_KERNEL_SRC='on'
+    ALLOW_KERNEL_CONFIG_CHANGES='off'
 
-echo "Build dir: ${current_dir}"
+    BUILD_ROOTFS='on'
+    ALLOW_ROOTFS_CHANGES='off'
 
-if [ ${BUILD_KERNEL} = 'yes' ]
-then
-    echo "Kernel ${kernel_branch}"
+    # Show user checklist to select
+    display_checklist "Build setup" "Select components and options for build:" \
+        "1" "Linux Kernel" "$BUILD_KERNEL" \
+        "2" "Clean Kernel sources" "$CLEAN_KERNEL_SRC" \
+        "3" "Allow Kernel config changes" "$ALLOW_KERNEL_CONFIG_CHANGES" \
+        "4" "Debian Rootfs" "$BUILD_ROOTFS" \
+        "5" "Allow Rootfs changes" "$ALLOW_ROOTFS_CHANGES" 
+        
+    # Accept user choices
+    BUILD_KERNEL='off'
+    CLEAN_KERNEL_SRC='off'
+    ALLOW_KERNEL_CONFIG_CHANGES='off'
+    BUILD_ROOTFS='off'
+    ALLOW_ROOTFS_CHANGES='off'
+
+    [[ $selection == *1* ]] && BUILD_KERNEL='on'
+    [[ $selection == *2* ]] && CLEAN_KERNEL_SRC='on'
+    [[ $selection == *3* ]] && ALLOW_KERNEL_CONFIG_CHANGES='on'
+    [[ $selection == *4* ]] && BUILD_ROOTFS='on'
+    [[ $selection == *5* ]] && ALLOW_ROOTFS_CHANGES='on'
 fi
 
-if [ ${BUILD_ROOT} = 'yes' ]
-then
-    echo "Rootfs ${release} , Hostname ${def_hostname}"
-    echo "Build initramfs: $BUILD_INITRAMFS"
-    echo "Allow rootfs changes: $ALLOW_ROOTFS_CHANGES"
+# only allow building initramfs if rootfs build is enabled
+if [[ BUILD_ROOTFS == "on" ]] && [[ -z $BUILD_INITRAMFS ]]; then
+    display_menu "Build initramfs" "Do you want to build the initramfs?" \
+    "1" "yes" \
+    "2" "no" 
+
+    [[ $selection == 1 ]] && BUILD_INITRAMFS='on'
 fi
 
-
-sleep 5
-echo '### Starting build'
-
-if [ ${BUILD_KERNEL} = 'yes' ]
-then
-    build_kernel
-fi
-if [ ${BUILD_ROOT} = 'yes' ]
-then
-   build_root_fs
-fi
-echo "### Done"
+[[ $BUILD_KERNEL == "on" ]] && build_kernel
+[[ $BUILD_ROOTFS == "on" ]] && build_root_fs
 
