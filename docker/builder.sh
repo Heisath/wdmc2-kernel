8ec9448f44dfa3d68416e90e23a9a29130cf5acd
cory <cory@docker-builder.host>
2023-06-09 00:23:24 -0500

    update: rename script

--- docker/builder.sh
+++ docker/builder.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-podman build \
+docker build \
   -t localhost/mycloud-builder \
   -f docker/Dockerfile
 
-podman rm mycloud-builder -f
+docker rm mycloud-builder -f
 
-podman run -it --rm \
+docker run -it --rm \
   --name mycloud-builder \
   -v $(pwd):/build:z \
   --privileged \
